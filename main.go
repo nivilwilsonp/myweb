86d0a1ba7a4c833b50139603f1fcf59d613d7ddf
nivilwilsonp <nivilwilsonp@gmail.com>
2022-12-16 11:31:26 +0530

    loading files

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/heroku/x/hmetrics/onload"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.LoadHTMLGlob("templates/*.tmpl.html")
+	router.Static("/Static", "static")
+	router.StaticFile("/app-ads.txt", "./app-ads.txt")
+	router.StaticFile("/privacy2.txt", "./privacy2.txt")
+	router.StaticFile("/privacy3.txt", "./privacy3.txt")
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+	})
+
+	router.Run(":" + port)
+}
